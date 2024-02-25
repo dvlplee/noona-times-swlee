817a9d4d35ce6bc47f895765827946bd9ba6b407
Seungwon Lee <dvlplee16@gmail.com>
2024-02-26 04:32:54 +0900

    final

--- main.js
+++ main.js
@@ -115,21 +115,39 @@ const paginationRender = () => {
   //pageGroup
   const pageGroup = Math.ceil(page / groupSize);
   //lastpage
-  const lastPage = pageGroup * groupSize;
+  //   let lastPage = pageGroup * groupSize;
   //마지막 페이지그룹이 그룹사이즈보다 작다? lastpage = totalpage
+  let lastPage = pageGroup * 5;
   if (lastPage > totalPages) {
     lastPage = totalPages;
   }
   //firstpage
-  const firstPage =
-    lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
-
+  //   const firstPage =
+  //     lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+  let firstPage = lastPage - 4 <= 0 ? 1 : lastPage - 4; // 첫그룹이 5이하이면
   let paginationHTML = ``;
+  if (firstPage >= 6) {
+    paginationHTML = `<li class="page-item" onclick="moveToPage(1)">
+    <a class="page-link" href='#js-bottom'>&lt;&lt;</a>
+  </li>
+  <li class="page-item" onclick="moveToPage(${page - 1})">
+    <a class="page-link" href='#js-bottom'>&lt;</a>
+  </li>`;
+  }
 
-  for (let i = firstPage; i < lastPage; i++) {
-    paginationHTML += `<li class="page-item ${
-      i === page ? "active" : ""
-    }" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+  for (let i = firstPage; i <= lastPage; i++) {
+    paginationHTML += `<li class="page-item ${i == page ? "active" : ""}" >
+    <a class="page-link" href='#js-bottom' id='page-${i}' onclick="moveToPage(${i})" >${i}</a>
+   </li>`;
+  }
+
+  if (lastPage < totalPages) {
+    paginationHTML += `<li class="page-item" onclick="moveToPage(${page + 1})">
+    <a  class="page-link" href='#js-program-detail-bottom'>&gt;</a>
+   </li>
+   <li class="page-item" onclick="moveToPage(${totalPages})">
+    <a class="page-link" href='#js-bottom'>&gt;&gt;</a>
+   </li>`;
   }
   document.querySelector(".pagination").innerHTML = paginationHTML;
 };
@@ -162,7 +180,7 @@ function toggleSearch() {
 
 // 햄버거 버튼 클릭시 side메뉴 나오게 하기
 function toggleMenu() {
-  var sideMenu = document.getElementById("side-menu");
+  let sideMenu = document.getElementById("side-menu");
   sideMenu.style.display =
     sideMenu.style.display === "none" || sideMenu.style.display === ""
       ? "block"
